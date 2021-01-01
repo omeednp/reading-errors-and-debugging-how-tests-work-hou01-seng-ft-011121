d566d1577e5af77bec756aa72424cbba505fba65
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-01 04:35:47 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
 # Add your variables here
+
+Float first_number = 1
+
+Float second_number = 5
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
